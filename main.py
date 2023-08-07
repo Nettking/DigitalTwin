9eeecb081b71f477c6db6d48e03cc3f8980eadc9
Nettking <m@nettking.no>
2023-08-08 00:24:34 +0200

    Added error handling for MQTT

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ if __name__ == '__main__':
     # Create a thread for the update_via_mqtt function
     mqtt_thread = threading.Thread(target=update_via_mqtt)
 
-    # Start the thread
-    mqtt_thread.start()
+    try:
+        # Start the thread
+        mqtt_thread.start()
+    except: 
+        print("Could not start MQTT thread.")
 
     # Continue with the rest of the program
     print_predictions()
     detect_leaves('/mnt/c/strawberries/Strawberry___Healthy')
 
-    # Wait for the mqtt_thread to finish before exiting the program
-    mqtt_thread.join()
+    try:
+        # Wait for the mqtt_thread to finish before exiting the program
+        mqtt_thread.join()
+    except:
+        print("Could not access MQTT thread")
