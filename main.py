4a7d69609af7e93818ed8b0a64b897643182b69d
Nettking <m@nettking.no>
2023-08-07 23:08:58 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from print_predictions import *
+from update_via_mqtt import *
+from detect_leaf import *
+from detect_leaves import *
+
+if __name__ == '__main__':
+    #print_predictions()
+    #update_via_mqtt()
+    #detect_leaf()
+    detect_leaves('/mnt/c/strawberries/Strawberry___Healthy')
